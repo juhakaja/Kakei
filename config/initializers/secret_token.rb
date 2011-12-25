fc11548e5577f803b01102ca766142d78fe08f35
Juha Kajava <juhakaja@gmail.com>
2011-12-25 17:57:53 +0900

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kakei::Application.config.secret_token = '5fccc2463377acd3150fa291877a89092219b1cc8421f60c51b7277a6f6c7a0eb669b2c73af0544668ecc79164fe737e0d049590e5199753ec231a86cec03eca'
